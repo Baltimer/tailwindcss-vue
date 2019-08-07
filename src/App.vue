70025472c69a57ffb0513b673a5feaa21fda7f58
Lluis Cifre <luis.cifre@ejuniper.com>
2019-08-07 18:03:01 +0200

    Started NavBar, disabled EsLint

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav" class="bg-blue-700">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="w-full h-full bg-gray-300">
+    <NavBar></NavBar>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    "NavBar": NavBar
   }
 }
-</style>
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="h-12 bg-blue-700 px-4 py-2 w-full flex justify-between">
+    <div class="py-2">
+      <div class="h-4 w-4 bg-red-300 rounded-lg"></div>
+    </div>
+    <div>
+      <router-link to="/" class="mr-2">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    
+  },
+};
+</script>
