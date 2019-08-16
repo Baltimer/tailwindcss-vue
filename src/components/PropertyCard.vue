68d2a65eb5d4257bccf686296ace995ce1f2789b
Lluis Cifre <luis.cifre@ejuniper.com>
2019-08-16 08:34:48 +0200

    Finished PropertyCard + FIX for image link

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -1,27 +1,25 @@
 <template>
     <div>
         <div class="relative pb-5/6">
-        <img class="absolute inset-0 h-full rounded-lg shadow-md object-cover" :src="require(property.imgUrl)" alt="home1">
+        <img class="absolute inset-0 h-full rounded-lg shadow-md object-cover" :src="property.imgUrl" alt="home1">
         </div>
         <div class="relative px-4 -mt-16">
         <div class="bg-white rounded-lg px-4 py-4 shadow-lg">
             <div class="flex">
             <span class="bg-teal-200 text-teal-800 font-medium uppercase tracking-wide text-xs rounded-full px-2 py-1 leading-none inline-block">PLUS</span>
             <div class="ml-2 text-xs text-gray-600 font-semibold uppercase tracking-wide">
-                {{property.beds}} camas &bull; {{property.baths}} baños
+                {{property.beds}} {{property.beds === 1 ? 'cama' : 'camas'}} &bull; {{property.baths}} {{property.baths === 1 ? 'baño' : 'baños'}}
             </div>
             </div>
-            <h4 class="mt-1 text-gray-900 font-semibold text-lg">Casa moderna en el centro</h4>
+            <h4 class="mt-1 text-gray-900 font-semibold text-lg">{{property.title}}</h4>
             <div class="mt-1">
-            <span class="text-gray-900">230€</span><span class="mt-1 text-sm text-gray-600">/día</span>
+            <span class="text-gray-900">{{formatedPrice}}</span><span class="mt-1 text-sm text-gray-600">/día</span>
             </div>
             <div class="text-sm text-gray-600 flex items-center mt-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24"><path d="M6.1 21.98a1 1 0 0 1-1.45-1.06l1.03-6.03-4.38-4.26a1 1 0 0 1 .56-1.71l6.05-.88 2.7-5.48a1 1 0 0 1 1.8 0l2.7 5.48 6.06.88a1 1 0 0 1 .55 1.7l-4.38 4.27 1.04 6.03a1 1 0 0 1-1.46 1.06l-5.4-2.85-5.42 2.85zm4.95-4.87a1 1 0 0 1 .93 0l4.08 2.15-.78-4.55a1 1 0 0 1 .29-.88l3.3-3.22-4.56-.67a1 1 0 0 1-.76-.54l-2.04-4.14L9.47 9.4a1 1 0 0 1-.75.54l-4.57.67 3.3 3.22a1 1 0 0 1 .3.88l-.79 4.55 4.09-2.15z"/></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24"><path d="M6.1 21.98a1 1 0 0 1-1.45-1.06l1.03-6.03-4.38-4.26a1 1 0 0 1 .56-1.71l6.05-.88 2.7-5.48a1 1 0 0 1 1.8 0l2.7 5.48 6.06.88a1 1 0 0 1 .55 1.7l-4.38 4.27 1.04 6.03a1 1 0 0 1-1.46 1.06l-5.4-2.85-5.42 2.85zm4.95-4.87a1 1 0 0 1 .93 0l4.08 2.15-.78-4.55a1 1 0 0 1 .29-.88l3.3-3.22-4.56-.67a1 1 0 0 1-.76-.54l-2.04-4.14L9.47 9.4a1 1 0 0 1-.75.54l-4.57.67 3.3 3.22a1 1 0 0 1 .3.88l-.79 4.55 4.09-2.15z"/></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24"><path d="M6.1 21.98a1 1 0 0 1-1.45-1.06l1.03-6.03-4.38-4.26a1 1 0 0 1 .56-1.71l6.05-.88 2.7-5.48a1 1 0 0 1 1.8 0l2.7 5.48 6.06.88a1 1 0 0 1 .55 1.7l-4.38 4.27 1.04 6.03a1 1 0 0 1-1.46 1.06l-5.4-2.85-5.42 2.85zm4.95-4.87a1 1 0 0 1 .93 0l4.08 2.15-.78-4.55a1 1 0 0 1 .29-.88l3.3-3.22-4.56-.67a1 1 0 0 1-.76-.54l-2.04-4.14L9.47 9.4a1 1 0 0 1-.75.54l-4.57.67 3.3 3.22a1 1 0 0 1 .3.88l-.79 4.55 4.09-2.15z"/></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24"><path d="M6.1 21.98a1 1 0 0 1-1.45-1.06l1.03-6.03-4.38-4.26a1 1 0 0 1 .56-1.71l6.05-.88 2.7-5.48a1 1 0 0 1 1.8 0l2.7 5.48 6.06.88a1 1 0 0 1 .55 1.7l-4.38 4.27 1.04 6.03a1 1 0 0 1-1.46 1.06l-5.4-2.85-5.42 2.85zm4.95-4.87a1 1 0 0 1 .93 0l4.08 2.15-.78-4.55a1 1 0 0 1 .29-.88l3.3-3.22-4.56-.67a1 1 0 0 1-.76-.54l-2.04-4.14L9.47 9.4a1 1 0 0 1-.75.54l-4.57.67 3.3 3.22a1 1 0 0 1 .3.88l-.79 4.55 4.09-2.15z"/></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24"><path d="M6.1 21.98a1 1 0 0 1-1.45-1.06l1.03-6.03-4.38-4.26a1 1 0 0 1 .56-1.71l6.05-.88 2.7-5.48a1 1 0 0 1 1.8 0l2.7 5.48 6.06.88a1 1 0 0 1 .55 1.7l-4.38 4.27 1.04 6.03a1 1 0 0 1-1.46 1.06l-5.4-2.85-5.42 2.85zm4.95-4.87a1 1 0 0 1 .93 0l4.08 2.15-.78-4.55a1 1 0 0 1 .29-.88l3.3-3.22-4.56-.67a1 1 0 0 1-.76-.54l-2.04-4.14L9.47 9.4a1 1 0 0 1-.75.54l-4.57.67 3.3 3.22a1 1 0 0 1 .3.88l-.79 4.55 4.09-2.15z"/></svg>
-            <span class="ml-2">34 valoraciones</span>
+            <svg v-for="i in 5" :key="i" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56 53" class="h-4 w-4 fill-current" :class="property.rating > i ? 'text-teal-500' : 'text-gray-400'">
+                <path d="M48.856 22.73c.983-.958 1.33-2.364.906-3.671-.425-1.307-1.532-2.24-2.892-2.438l-12.092-1.757c-.515-.075-.96-.398-1.19-.865L28.182 3.043c-.607-1.231-1.839-1.996-3.212-1.996-1.372 0-2.604.765-3.211 1.996L16.352 14c-.23.467-.676.79-1.191.865L3.069 16.622C1.71 16.819.602 17.753.177 19.06c-.424 1.307-.077 2.713.906 3.671l8.749 8.528c.373.364.544.888.456 1.4L8.224 44.701c-.183 1.06.095 2.091.781 2.904 1.066 1.267 2.927 1.653 4.415.871l10.814-5.686c.452-.237 1.021-.235 1.472 0l10.815 5.686c.526.277 1.087.417 1.666.417 1.057 0 2.059-.47 2.748-1.288.687-.813.964-1.846.781-2.904l-2.065-12.042c-.088-.513.083-1.036.456-1.4l8.749-8.529z"/>
+            </svg>
+            <span class="ml-2">{{property.reviewCount}} valoraciones</span>
             </div>
         </div>
         </div>
@@ -30,6 +28,16 @@
 
 <script>
 export default {
-    props: ['property']
+    props: ['property'],
+    computed: {
+        formatedPrice() {
+            const formatter = new Intl.NumberFormat('es-ES', {
+                style: 'currency',
+                currency: 'EUR'
+            });
+
+            return formatter.format(this.property.price);
+        }
+    }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,18 @@
   <main class="py-6 xl:flex-1 xl:overflow-x-hidden">
     <div class="px-4">
       <h3 class="text-gray-900 text-xl">Casas</h3>
-      <p class="text-gray-600">Encuentra las casas que necesites!</p>
+      <p class="text-gray-600">Encuentra las casas que necesites.</p>
+    </div>
+    <div class="mt-6 sm:overflow-x-auto">
+      <div class="px-4 sm:inline-flex sm:-ml-2 sm:pb-8">
+        <div v-for="(property, i) in properties" :key="i" :class="{ 'mt-10': i > 0 }" class="sm:mt-0 sm:max-w-xs sm:w-80 sm:flex-shrink-0 sm:px-2">
+          <PropertyCard :property="property" />
+        </div>
+      </div>
+    </div>
+    <div class="px-4">
+      <h3 class="text-gray-900 text-xl">Villas</h3>
+      <p class="text-gray-600">Las mejores villas a tu disposición.</p>
     </div>
     <div class="mt-6 sm:overflow-x-auto">
       <div class="px-4 sm:inline-flex sm:-ml-2 sm:pb-8">
@@ -26,39 +37,39 @@ export default {
     return {
       properties: [
         {
-          imgUrl: "./assets/img/home1.jpg",
+          imgUrl: "https://images.unsplash.com/photo-1543489822-c49534f3271f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
           beds: 3,
           baths: 2,
           title: "Casa moderna en el centro",
-          price: 140000,
+          price: 300,
           reviewCount: 34,
           rating: 4
         },
         {
-          imgUrl: "./assets/img/home1.jpg",
+          imgUrl: "https://images.unsplash.com/photo-1553524789-59ac0ed1d2d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
           beds: 3,
-          baths: 2,
+          baths: 1,
           title: "Casa moderna en el centro",
-          price: 140000,
-          reviewCount: 34,
-          rating: 4
+          price: 200,
+          reviewCount: 19,
+          rating: 3
         },
         {
-          imgUrl: "../img/home1.jpg",
-          beds: 3,
-          baths: 2,
+          imgUrl: "https://images.unsplash.com/photo-1543489822-c49534f3271f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
+          beds: 1,
+          baths: 1,
           title: "Casa moderna en el centro",
-          price: 140000,
-          reviewCount: 34,
-          rating: 4
+          price: 280,
+          reviewCount: 24,
+          rating: 5
         },
         {
-          imgUrl: "./assets/img/home1.jpg",
+          imgUrl: "https://images.unsplash.com/photo-1553524805-28dbfc2f84fd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1491&q=80",
           beds: 3,
           baths: 2,
           title: "Casa moderna en el centro",
-          price: 140000,
-          reviewCount: 34,
+          price: 220,
+          reviewCount: 37,
           rating: 4
         }
       ]
